505662211e097620c9dbbe4d8beac079d60e1f28
SverreNystad <sverrenystad@gmail.com>
2024-09-29 19:35:07 +0200

    build: Update SECRET_KEY handling in settings.py and use postgres latest

--- src/tutorai/settings.py
+++ src/tutorai/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-f=vaz!(p7-=5(ufwm+*dg18-7a^d_^gxmmmq=ovx2n=+vsv8=p"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-f=vaz!(p7-=5(ufwm+*dg18-7a^d_^gxmmmq=ovx2n=+vsv8=p")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
