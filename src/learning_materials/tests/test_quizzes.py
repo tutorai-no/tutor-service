1408c6994af75e1be123f308d5e739046f41a963
SverreNystad <sverrenystad@gmail.com>
2024-09-29 11:29:55 +0200

    test: Put tests into test folder

---
+++ src/learning_materials/tests/test_quizzes.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from unittest.mock import patch
+from learning_materials.learning_resources import Quiz, QuestionAnswer, MultipleChoiceQuestion, Page
+from learning_materials.quizzes.quiz_service import generate_quiz
+
+
+class QuizGenerationTests(TestCase):
+   pass
+
+class QuizGradingTests(TestCase):
+   pass
