63e6b56bcf9d87006a78e3e9034d1bfefc7a24d1
SverreNystad <sverrenystad@gmail.com>
2024-10-01 14:38:06 +0200

    feat: create CustomUser with subscriptions and history as well as CustomUserAdmin

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from accounts.views import CustomTokenObtainPairView, LogoutView, PasswordResetConfirmView, PasswordResetView, RegisterView, UserProfileView
+from accounts.views import CustomTokenObtainPairView, LogoutView, PasswordResetConfirmView, PasswordResetView, RegisterView, SubscriptionListView, UserProfileView, SubscriptionHistoryView
 from api.views import health_check
 from learning_materials.views import (
     FlashcardCreationView,
@@ -30,6 +30,10 @@ urlpatterns = [
     path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
     path('password-reset-confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('profile/', UserProfileView.as_view(), name='profile'),
+
+    path('subscriptions/', SubscriptionListView.as_view(), name='subscriptions'),
+    path('subscription-history/', SubscriptionHistoryView.as_view(), name='subscription_history'),
+
 
  
 ]
--- src/tutorai/settings.py
+++ src/tutorai/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
     }
 }
 
-# AUTH_USER_MODEL = "users.User"
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
