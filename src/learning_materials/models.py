f649a1b1b8feca25a4eadcc7f94645d30e05510f
SverreNystad <sverrenystad@gmail.com>
2024-10-06 00:20:58 +0200

    feat: Add QuizModel to learning materials

--- src/learning_materials/models.py
+++ src/learning_materials/models.py
@@ -1,18 +1,6 @@
 from django.db import models
 
 
-class FlashcardModel(models.Model):
-    """Model to store flashcards"""
-    id = models.AutoField(primary_key=True)
-    front = models.TextField(help_text="The front of the flashcard")
-    back = models.TextField(help_text="The back of the flashcard")
-    cardset = models.ForeignKey(
-        "Cardset", on_delete=models.CASCADE, help_text="The cardset to which the flashcard belongs"
-    )
-
-    def __str__(self):
-        return self.front
-
 class Cardset(models.Model):
     """Model to store cardsets"""
     id = models.AutoField(primary_key=True)
@@ -22,3 +10,50 @@ class Cardset(models.Model):
 
     def __str__(self):
         return self.name
+
+class FlashcardModel(models.Model):
+    """Model to store flashcards"""
+    id = models.AutoField(primary_key=True)
+    front = models.TextField(help_text="The front of the flashcard")
+    back = models.TextField(help_text="The back of the flashcard")
+    cardset = models.ForeignKey(
+        Cardset, on_delete=models.CASCADE, help_text="The cardset to which the flashcard belongs"
+    )
+
+    def __str__(self):
+        return self.front
+
+class QuizModel(models.Model):
+    """Model to store quizzes"""
+    id = models.AutoField(primary_key=True)
+    document_name = models.CharField(max_length=100, help_text="The name of the document", default="unknown")
+    start = models.IntegerField(help_text="The starting page of the quiz", default=1)
+    end = models.IntegerField(help_text="The ending page of the quiz", default=1)
+
+    def __str__(self):
+        return f"Quiz for {self.document_name} from page {self.start} to {self.end}"
+
+class QuestionAnswerModel(models.Model):
+    """Model to store question-answer pairs"""
+    id = models.AutoField(primary_key=True)
+    question = models.TextField(help_text="The question part of the QA pair")
+    answer = models.TextField(help_text="The answer part of the QA pair")
+    quiz = models.ForeignKey(
+        QuizModel, on_delete=models.CASCADE, help_text="The quiz to which the question-answer pair belongs"
+    )
+
+    def __str__(self):
+        return self.question
+    
+class MultipleChoiceQuestionModel(models.Model):
+    """Model to store multiple-choice questions"""
+    id = models.AutoField(primary_key=True)
+    question = models.TextField(help_text="The question part of the multiple-choice question")
+    options = models.JSONField(help_text="The list of options to choose from")
+    answer = models.TextField(help_text="The correct answer to the question")
+    quiz = models.ForeignKey(
+        QuizModel, on_delete=models.CASCADE, help_text="The quiz to which the multiple-choice question belongs"
+    )
+
+    def __str__(self):
+        return self.question
