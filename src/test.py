20f3e3254e4480e70f5caf0b8364fc1a76da7b2b
SverreNystad <sverrenystad@gmail.com>
2024-10-18 22:56:36 +0200

    refactor: Remove unused files from the project

--- src/test.py
+++
@@ -1,7 +0,0 @@
-from learning_materials.knowledge_base.db_interface import MongoDB
-
-if __name__ == '__main__':
-    db = MongoDB()
-    result = db.get_page_range("Assignment 4.pdf", 1, 3)
-    print(result)
-    print(len(result))
