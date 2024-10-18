835bca5a892d4f1c250b535beb6fe1ec0e56fdf6
SverreNystad <sverrenystad@gmail.com>
2024-10-18 22:48:31 +0200

    test: Add test for health check endpoint

--- src/api/tests.py
+++ src/api/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class TestHealthCheck(TestCase):
+    def test_health_check(self):
+        url = reverse("health-check")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
