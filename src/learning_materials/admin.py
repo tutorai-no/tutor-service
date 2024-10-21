93310a6acb673761cbe72383675436242562203a
SverreNystad <sverrenystad@gmail.com>
2024-10-21 16:00:46 +0200

    feat: make flashcards show up on cardset

--- src/learning_materials/admin.py
+++ src/learning_materials/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 
 from learning_materials.models import FlashcardModel, Cardset
 
-# Register your models here.
-admin.site.register(FlashcardModel)
-admin.site.register(Cardset)
+
+class FlashcardInline(admin.TabularInline):
+    model = FlashcardModel
+    fields = ["id", "front", "back", "proficiency"]
+    readonly_fields = ["id", "time_of_next_review"]
+    extra = 0
+
+
+# Add cardset with flashcards to the admin site
+@admin.register(Cardset)
+class CardsetAdmin(admin.ModelAdmin):
+    model = Cardset
+    list_display = [
+        "id",
+        "name",
+        "description",
+        "subject",
+        "user",
+    ]
+    search_fields = ["name", "subject", "user__username"]
+    list_filter = ["subject", "user"]
+
+    inlines = [FlashcardInline]
