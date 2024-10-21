3a3550ce96bea1e9aa2db00226162c6f14198493
SverreNystad <sverrenystad@gmail.com>
2024-10-21 16:05:44 +0200

    feat: add time of next review to cardset admin page and flashcard model to admin panel

--- src/learning_materials/admin.py
+++ src/learning_materials/admin.py
@@ -5,8 +5,8 @@ from learning_materials.models import FlashcardModel, Cardset
 
 class FlashcardInline(admin.TabularInline):
     model = FlashcardModel
-    fields = ["id", "front", "back", "proficiency"]
-    readonly_fields = ["id", "time_of_next_review"]
+    fields = ["front", "back", "proficiency", "time_of_next_review"]
+    readonly_fields = ["time_of_next_review"]
     extra = 0
 
 
@@ -25,3 +25,6 @@ class CardsetAdmin(admin.ModelAdmin):
     list_filter = ["subject", "user"]
 
     inlines = [FlashcardInline]
+
+
+admin.site.register(FlashcardModel)
