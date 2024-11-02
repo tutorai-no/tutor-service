03c5daa62a9605b8e5b288ee6ec420ea8f51a8ac
olavsl <olav.selnes.lorentzen@hotmail.com>
2024-11-02 10:52:23 +0100

    refactor db search

--- src/learning_materials/knowledge_base/embeddings.py
+++ src/learning_materials/knowledge_base/embeddings.py
@@ -21,7 +21,7 @@ class EmbeddingsModel(ABC):
 
 
 class OpenAIEmbedding(EmbeddingsModel):
-    def __init__(self, model_name: str = "text-embedding-ada-002"):
+    def __init__(self, model_name: str = "text-embedding-3-small"): # TODO: Make sure the model is the same as used in tango-scraper
         api_key = Config().API_KEY
         self.client = openai.Client(api_key=api_key)
         self.model_name = model_name
