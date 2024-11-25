1d3df9c0696998e1e094b0bf5dbd0f92bddaf45d
olavsl <olav.selnes.lorentzen@hotmail.com>
2024-11-25 12:38:24 +0100

    Refactor: Correct uploading of files

--- src/learning_materials/files/file_service.py
+++ src/learning_materials/files/file_service.py
@@ -1,6 +1,9 @@
-from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPermissions
+import os
+from typing import Tuple
+from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPermissions, ContentSettings
 from datetime import datetime, timedelta
 from django.core.files.uploadedfile import UploadedFile
+from django.utils.text import get_valid_filename
 from uuid import UUID
 from config import Config
 
@@ -11,22 +14,25 @@ AZURE_CONTAINER_NAME = config.AZURE_STORAGE_CONTAINER_NAME
 blob_service_client = BlobServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
 container_client = blob_service_client.get_container_client(AZURE_CONTAINER_NAME)
 
-def upload_file_to_blob(file: UploadedFile, user_uuid: UUID, course_uuid: UUID, file_uuid: UUID) -> str:
+def upload_file_to_blob(file: UploadedFile, user_uuid: UUID, course_uuid: UUID, file_uuid: UUID) -> Tuple[str, str]:
     """
-    Uploads a file to Azure Blob Storage and returns the URL.
+    Uploads a file to Azure Blob Storage and returns the blob name and URL.
 
     Args:
         file (UploadedFile): The uploaded file.
         user_uuid (UUID): The UUID of the user.
         course_uuid (UUID): The UUID of the course.
+        file_uuid (UUID): The UUID of the file.
 
     Returns:
-        str: The URL of the uploaded file in Azure Blob Storage.
+        Tuple[str, str]: The blob name and the URL of the uploaded file in Azure Blob Storage.
     """
-    blob_name = f"{user_uuid}/{course_uuid}/{file_uuid}"
+    _, file_extension = os.path.splitext(file.name)
+    blob_name = f"{user_uuid}/{course_uuid}/{file_uuid}{file_extension}"
     blob_client = container_client.get_blob_client(blob_name)
-    blob_client.upload_blob(file, overwrite=True)
-    return blob_client.url
+    content_settings = ContentSettings(content_type=file.content_type)
+    blob_client.upload_blob(file, overwrite=True, content_settings=content_settings)
+    return blob_name, blob_client.url
 
 def generate_sas_url(blob_name: str) -> str:
     sas_token = generate_blob_sas(
