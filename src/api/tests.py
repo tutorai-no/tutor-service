24209b41c7e1d998cc428a1da78d964e483554c5
SverreNystad <sverrenystad@gmail.com>
2024-12-10 19:29:54 +0100

    test: check that health check checks if rag database is reachable

--- src/api/tests.py
+++ src/api/tests.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 from django.test import TestCase
 from django.urls import reverse
 
@@ -8,3 +9,11 @@ class TestHealthCheck(TestCase):
         url = reverse("health-check")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+
+    @patch("learning_materials.knowledge_base.db_interface.MongoDB.is_reachable")
+    def test_health_check_database_down(self, mock_is_reachable):
+        mock_is_reachable.return_value = False
+        url = reverse("health-check")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 503)
+        self.assertEqual(response.content, b'"RAG Database is unreachable"')
