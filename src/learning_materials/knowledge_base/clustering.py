d5c19a8dcdfb61ff6250324b41c490b31f397882
Kristoffer Olaisen <knolaisen@gmail.com>
2025-01-10 16:36:29 +0100

    feat: embedding to 2d projection function

--- src/learning_materials/knowledge_base/clustering.py
+++ src/learning_materials/knowledge_base/clustering.py
@@ -1,4 +1,6 @@
 from sklearn.cluster import KMeans
+from sklearn.manifold import TSNE
+import numpy as np
 
 
 def cluster_embeddings(embeddings: list[list[float]], n_clusters: int = 5) -> list[int]:
@@ -15,3 +17,17 @@ def cluster_embeddings(embeddings: list[list[float]], n_clusters: int = 5) -> li
     kmeans = KMeans(n_clusters=n_clusters, init="k-means++", random_state=42)
     kmeans.fit(embeddings)
     return kmeans.labels_.tolist()
+
+def create_2d_projection(embeddings: list[list[float]]) -> list[list[float]]:
+    """
+    Create a 2D projection of the embeddings using PCA
+
+    Args:
+        embeddings (list[list[float]]): The embeddings to be projected
+
+    Returns:
+        list[list[float]]: The 2D projection of the embeddings
+    """
+    tsne = TSNE(n_components=2, perplexity=5, random_state=42, init="random", learning_rate=200)
+    embeddings = np.array(embeddings)
+    return tsne.fit_transform(embeddings).tolist()
--- src/learning_materials/tests/test_knowledge_base.py
+++ src/learning_materials/tests/test_knowledge_base.py
@@ -1,5 +1,5 @@
 from learning_materials.knowledge_base import factory
-from learning_materials.knowledge_base.clustering import cluster_embeddings
+from learning_materials.knowledge_base.clustering import cluster_embeddings, create_2d_projection
 from learning_materials.knowledge_base.embeddings import EmbeddingsModel
 from learning_materials.knowledge_base.response_formulation import generate_name_for_cluster    
 from django.test import TestCase
@@ -50,3 +50,28 @@ class ClusteringNamingTest(TestCase):
         cluster_name = generate_name_for_cluster(self.cluster_chunks)
         print(cluster_name)
         self.assertIsInstance(cluster_name, str)
+
+
+class ProjectionTest(TestCase):
+    def setUp(self):
+        self.embedding_model: EmbeddingsModel = factory.create_embeddings_model()
+        self.texts = [
+            "Why don't scientists trust atoms? Because they make up everything!",
+            "Why did the scarecrow win an award? Because he was outstanding in his field!",
+            "Why don’t skeletons fight each other? They don’t have the guts.",
+            "What do you call cheese that isn't yours? Nacho cheese.",
+            "Why couldn't the bicycle stand up by itself? It was two-tired.",
+            "What do you call fake spaghetti? An impasta!",
+            "Why did the math book look sad? Because it had too many problems.",
+            "What do you call a bear with no teeth? A gummy bear.",
+            "Why don’t oysters donate to charity? Because"]
+        self.embeddings = [
+            self.embedding_model.get_embedding(text) for text in self.texts
+        ]
+
+    def test_projection(self):
+        projection = create_2d_projection(self.embeddings)
+        print(projection)
+        self.assertTrue(all(isinstance(point, list) for point in projection))
+        self.assertEqual(len(projection[0]), 2)
+        self.assertEqual(len(projection), len(self.embeddings))
