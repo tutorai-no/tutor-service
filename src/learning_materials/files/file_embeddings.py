fe860e0056019e0abd1f8562372b2210d6a0cb4b
tobiasfremming <tobias.fremming@gmail.com>
2025-06-17 16:47:21 +0200

    clean: remove commented-out code for file embeddings function

--- src/learning_materials/files/file_embeddings.py
+++ src/learning_materials/files/file_embeddings.py
@@ -5,26 +5,6 @@ config = Config()
 BASE_URL_SCRAPER = config.BASE_URL_SCRAPER
 
 
-# def create_file_embeddings(files, file_uuids: str, auth_header: str) -> dict:
-#     endpoint = f"{BASE_URL_SCRAPER}/file/"
-#     file.seek(0)
-#     file_content = file.read()
-
-#     files = {"files": (file.name, file_content, file.content_type)}
-
-#     data = {"uuids": file_uuids}
-
-#     headers = {"Authorization": auth_header}
-
-#     response = requests.post(endpoint, headers=headers, files=files, data=data)
-
-#     if response.status_code != 200:
-#         raise Exception(
-#             f"Request failed with status code {response.status_code}: {response.text}"
-#         )
-
-#     return response.json()
-
 import asyncio
 from typing import Iterable, Union, IO, List, Tuple
 import requests
